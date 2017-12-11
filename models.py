5d18ea96e60be6e9d565ff970804f2d9d7f5bbfa
Diana Rojas <dianacarolinarojas@gmail.com>
2017-12-11 10:32:54 -0500

    Se agregó evento para journal_id, como evento onchange_journal pero reemplazando el llamado del evento onchange_partner_id por evento onchange_nro_cupon.

--- models.py
+++ models.py
@@ -144,6 +144,52 @@ class account_voucher(models.Model):
     #         res['value']['paid_amount_in_company_currency'] = amount_in_company_currency
     #     return res
 
+    def onchange_journal_filters(self, cr, uid, ids, journal_id, line_ids, tax_id, partner_id, date, amount, ttype, company_id, line_type_filters, context=None):
+        if context is None:
+            context = {}
+        if not journal_id:
+            return False
+        journal_pool = self.pool.get('account.journal')
+        journal = journal_pool.browse(cr, uid, journal_id, context=context)
+        if ttype in ('sale', 'receipt'):
+            account_id = journal.default_debit_account_id
+        elif ttype in ('purchase', 'payment'):
+            account_id = journal.default_credit_account_id
+        else:
+            account_id = journal.default_credit_account_id or journal.default_debit_account_id
+        tax_id = False
+        if account_id and account_id.tax_ids:
+            tax_id = account_id.tax_ids[0].id
+
+        vals = {'value':{} }
+        if ttype in ('sale', 'purchase'):
+            vals = self.onchange_price(cr, uid, ids, line_ids, tax_id, partner_id, context)
+            vals['value'].update({'tax_id':tax_id,'amount': amount})
+        currency_id = False
+        if journal.currency:
+            currency_id = journal.currency.id
+        else:
+            currency_id = journal.company_id.currency_id.id
+
+        period_ids = self.pool['account.period'].find(cr, uid, dt=date, context=dict(context, company_id=company_id))
+        vals['value'].update({
+            'currency_id': currency_id,
+            'payment_rate_currency_id': currency_id,
+            'period_id': period_ids and period_ids[0] or False
+        })
+        #in case we want to register the payment directly from an invoice, it's confusing to allow to switch the journal
+        #without seeing that the amount is expressed in the journal currency, and not in the invoice currency. So to avoid
+        #this common mistake, we simply reset the amount to 0 if the currency is not the invoice currency.
+        if context.get('payment_expected_currency') and currency_id != context.get('payment_expected_currency'):
+            vals['value']['amount'] = 0
+            amount = 0
+        if partner_id:
+            #res = self.onchange_partner_id(cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, context)
+            res = self.onchange_nro_cupon(cr, uid, ids, partner_id, journal_id, amount, currency_id, ttype, date, line_type_filters, context=context)
+            for key in res.keys():
+                vals[key].update(res[key])
+        return vals
+
     def get_strFilters_lines(self, filters, context=None):
         strCondition = ''
         if not context or context is None or len(context)==0 or len(filters)==0:
